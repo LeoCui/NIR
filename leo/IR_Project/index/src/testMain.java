b90ac5b6b20f284e70e8318ec244df3a3143f9a3
cuiyiming01 <cuiyiming01@baidu.com>
2018-01-06 17:49:31 +0800

    update

---
+++ leo/IR_Project/index/src/testMain.java
@@ -0,0 +1,11 @@
+
+import postingList.postingListSystem;
+
+public class testMain {
+	public static void main(String[] args) {
+		
+		postingListSystem postingListSystem = new postingListSystem();
+		postingListSystem.buildPostingList();
+		
+	}
+}
